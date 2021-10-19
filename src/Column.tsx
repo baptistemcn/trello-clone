31869eca7f272f95ce45ea9de7580d9ec8442b4b
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-19 18:31:45 +0200

    feat: Column Component v2

--- src/Column.tsx
+++ src/Column.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
+  children: React.ReactNode;
   text?: String;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ children, text }: ColumnProps) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };
