d66798d5857859ed13926a45fb9760d6e58ee663
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-20 18:22:23 +0200

    feat: react-dnd provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend as Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import { App } from "./App";
 import { AppStateProvider } from "./state/AppStateContext";
 
 ReactDOM.render(
   <AppStateProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <DndProvider backend={Backend}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </DndProvider>
   </AppStateProvider>,
   document.getElementById("root")
 );
