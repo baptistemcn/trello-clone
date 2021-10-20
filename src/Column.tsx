a9b5c21a6c5c7368d4546a5f7ae72bfdac29cbb2
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-20 19:48:08 +0200

    feat: useRef drag column

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { useAppState } from "./state/AppStateContext";
 import { addTask } from "./state/actions";
+import { useItemDrag } from "./utils/useItemDrag";
 
 type ColumnProps = {
   id: string;
@@ -14,8 +15,15 @@ type ColumnProps = {
 export const Column = ({ id, text }: ColumnProps) => {
   const { getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({
+    type: "COLUMN",
+    id,
+    text: "",
+  });
+  drag(ref);
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card text={task.text} key={task.id} id={task.id} />
