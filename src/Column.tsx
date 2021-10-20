e295028b93c7ffd05bc41628db2606e586314979
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-20 19:54:02 +0200

    feat: move the column

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,10 +1,11 @@
 import React, { useRef } from "react";
+import { useDrop } from "react-dnd";
 
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { useAppState } from "./state/AppStateContext";
-import { addTask } from "./state/actions";
+import { addTask, moveList } from "./state/actions";
 import { useItemDrag } from "./utils/useItemDrag";
 
 type ColumnProps = {
@@ -13,15 +14,30 @@ type ColumnProps = {
 };
 
 export const Column = ({ id, text }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState();
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
   const ref = useRef<HTMLDivElement>(null);
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover() {
+      if (!draggedItem) {
+        return;
+      }
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return;
+        }
+        dispatch(moveList(draggedItem.id, id));
+      }
+    },
+  });
   const { drag } = useItemDrag({
     type: "COLUMN",
     id,
     text: "",
   });
-  drag(ref);
+  drag(drop(ref));
+
   return (
     <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
