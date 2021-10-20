51f3a75ea7e106901bffbe62b1cd420cc229c4b7
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-20 14:17:11 +0200

    feat: onKeyPress submit

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { NewItemButton, NewItemFormContaner, NewItemInput } from "./styles";
+import { useFocus } from "./utils/useFocus";
 
 type NewItemFormProps = {
   onAdd(text: string): void;
@@ -8,9 +9,20 @@ type NewItemFormProps = {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
   return (
     <NewItemFormContaner>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleAddText}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContaner>
   );
